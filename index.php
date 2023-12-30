8fc72aff493a9ae851e556b5cbde283621cf86a8
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2023-12-30 12:14:59 +0100

    updated the selling mechanism.

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
 session_start();
 $allCards = file_get_contents("card.json"); // Get the contents of the JSON file
 $allCards2 = json_decode($allCards, true); // Decode the JSON into an associative array
+$allUsers = json_decode(file_get_contents("user.json"), true);
+
+// Check if the user is logged in
+if (isset($_SESSION["username"])) {
+    $users = $allUsers;
+    $currentUser = array_filter($users, function ($user) {
+        return $user["username"] === $_SESSION["username"];
+    });
+
+    // Since array_filter preserves array keys, we get the first element
+    $currentUser = reset($currentUser);
+    $money = $currentUser ? $currentUser["money"] : 0;
+}
+
+// Pagination setup
+$cardsPerPage = 10;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$page = max($page, 1);
+$offset = ($page - 1) * $cardsPerPage;
+// Slice the cards array to get just the subset of cards for the current page
+$displayCards = array_slice($allCards2, $offset, $cardsPerPage);
+
+// Calculate the total number of pages
+$totalPages = ceil(count($allCards2) / $cardsPerPage);
+
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +46,10 @@ $allCards2 = json_decode($allCards, true); // Decode the JSON into an associativ
     <?php if(isset($_SESSION["username"])): ?>
         <a href="user.php"><img href="user.php" src="/sprites/login.png" alt="User"></a>
         <a href="user.php"><?php echo $_SESSION["username"];?></a>
-        <a                ><?php echo $_SESSION["username"];?></a>
+        <a                ><?php echo "₽ $money";?></a>
+        <?php if($_SESSION["username"]=="admin"): ?>
+            <a href="card2.php">Create</a>
+        <?php endif?>
         <a href="logout.php">Logout</a>
     <?php else: ?>
         <a href="login.php"><img src="/sprites/login.png" alt="Login">Login</a>
@@ -30,7 +58,7 @@ $allCards2 = json_decode($allCards, true); // Decode the JSON into an associativ
 </div>
 
 <div class="card-placer">
-    <?php foreach ($allCards2 as $card): ?>
+    <?php foreach ($displayCards as $card): ?>
         <div class="card">
             <div class="card-image <?= $card["type"];?>">
                 <a href="card.php?card=<?= $card["name"]; ?>">
@@ -60,6 +88,19 @@ $allCards2 = json_decode($allCards, true); // Decode the JSON into an associativ
         </div>
     <?php endforeach; ?>
 </div>
+
+<div class="pagination">
+    <?php if ($page > 1): ?>
+        <a href="?page=<?= $page - 1 ?>">&laquo; Previous</a>
+    <?php endif; ?>
+    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+        <a href="?page=<?= $i ?>" class="<?= $page === $i ? 'active' : '' ?>"><?= $i ?></a>
+    <?php endfor; ?>
+    <?php if ($page < $totalPages): ?>
+        <a href="?page=<?= $page + 1 ?>">Next &raquo;</a>
+    <?php endif; ?>
+</div>
+
 </body>
 <footer>
     <p>PokémonTCG • Mohammed Efaz • © • <img src="sprites/charizard2_.png"></p>
